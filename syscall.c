e210d7da96fb2b566d4918d3827ec45dd3fd3001
mahdi aghajani <mma137421@gmail.com>
2017-01-11 17:01:45 +0330

    Corrects syscall and add getPerformanceData to it

--- syscall.c
+++ syscall.c
@@ -141,6 +141,7 @@ static int (*syscalls[])(void) = {
         [SYS_link]    sys_link,
         [SYS_mkdir]   sys_mkdir,
         [SYS_close]   sys_close,
+        [SYS_getPerformanceData] sys_getPerformanceData,
 };
 
 void
